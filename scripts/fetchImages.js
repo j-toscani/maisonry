6504142311168efb2581168c063bb32ad73c4b41
Julian Toscani <julian_toscani@gmx.de>
2021-06-14 23:43:06 +0200

    put fetch and creation of images ion a module

--- scripts/fetchImages.js
+++ scripts/fetchImages.js
@@ -1,5 +1,22 @@
-export default async function fetchImages() {
-  const url = "https://picsum.photos/v2/list";
+import setUpImageElement from "./setUpImageElement.js";
+
+export default async function setUpImages(handleShuffle) {
+  try {
+    const response = await fetchImages();
+    handleShuffle(createImages(response));
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchImages() {
+  const url = "https://picsum.photos/v2/list?page=4&limit=20";
   const response = await fetch(url);
   return response.json();
+}
+
+export function createImages(jsonImages) {
+  return jsonImages.map((jsonImage, index) =>
+    setUpImageElement(jsonImage.download_url)
+  );
 }
