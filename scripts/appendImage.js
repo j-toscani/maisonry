c5e6fe08cca39dc14a98e959d7ca6816a76144be
Julian Toscani <julian_toscani@gmx.de>
2021-06-16 23:49:19 +0200

    remove unused files

--- scripts/appendImage.js
+++
@@ -1,3 +0,0 @@
-export default function appendImage(container, image) {
-  container.appendChild(image);
-}
--- scripts/getColumns.js
+++
@@ -1,3 +0,0 @@
-export default function getColumns(containerQuery = ".column:not(.hidden)") {
-  return document.querySelectorAll(containerQuery);
-}
